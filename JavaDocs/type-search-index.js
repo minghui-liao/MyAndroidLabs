1a90a41fde514cf2f0c6b64183ae92b33f7f0cf4
Minghui Liao <liao0026@algonguinlive.com>
2021-06-18 00:57:09 -0400

    Finished test cases and Javadocs

--- JavaDocs/type-search-index.js
+++ JavaDocs/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"algonquin.cst2335.liao0026","l":"ExampleInstrumentedTest"},{"p":"algonquin.cst2335.liao0026","l":"ExampleUnitTest"},{"p":"algonquin.cst2335.liao0026","l":"MainActivity"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"algonquin.cst2335.liao0026","l":"ExampleInstrumentedTest"},{"p":"algonquin.cst2335.liao0026","l":"ExampleUnitTest"},{"p":"algonquin.cst2335.liao0026","l":"MainActivity"},{"p":"algonquin.cst2335.liao0026","l":"MainActivityTest"}]
